24be282fd4bd8201cb595e50bdbde6483d65e028
RyanOnTheInside <7623207+ryanontheinside@users.noreply.github.com>
2026-06-02 18:50:21 -0400

    fix: tooltip box resizing

--- demos/realtime_motion_graph_web/web/components/Performance/AdvancedDrawer.tsx
+++ demos/realtime_motion_graph_web/web/components/Performance/AdvancedDrawer.tsx
@@ -244,7 +244,7 @@ export function AdvancedDrawer({ savedTab, unsavedDot }: Props = {}) {
         >
           {spread ? renderAllSections(savedTab) : renderTabBody(activeTab, savedTab)}
         </div>
-        <DrawerHelpBar spread={spread} />
+        <DrawerHelpBar />
       </div>
     </aside>
   );
